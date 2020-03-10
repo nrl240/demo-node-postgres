ca6d90c62d6281b1b46c74e638b601623d0c116d
Noelle Laureano <nrlaureano@gmail.com>
2020-03-10 17:21:48 -0400

    comments code

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 const { Client } = require('pg'); // same as `const Client = require('pg').Client`
 const postgresUrl = 'postgres://localhost/brooklyn_restaurants';
 
+// https://node-postgres.com/api/client
+
+// Create a new Client instance to connect to the database
+// The "bridge" between our node app and our Postgres db
 const client = new Client(postgresUrl);
 
-const connector = async () => {
-  try {
-    client.connect();
-    console.log('Successfully connected!');
-  } catch (error) {
-    console.log('Oh no! An Error!: ', error);
-  }
-};
-connector();
+// Make the connection (see documentation)
+client
+  .connect()
+  .then(() => console.log('---- Connected ----'))
+  .catch((error) => console.error('---- Connection error ----\n', error.stack))
 
+// Write function to fetch restaurants from database
 const fetchRestaurants = async () => {
+
+  // Query for restaurants
   try {
     const data = await client.query(`SELECT * FROM restaurants;`);
-    // console.log('\n data --> ', data, '\n');
+    // What does the data look like?
+    console.log('\n data --> ', data, '\n');
+
+    // Why not just pull the `rows` property off of the data (Result) object?
     console.log('\n data.rows --> ', data.rows, '\n');
-    // data.rows.forEach(row => {
-    //   console.log(row);
-    // });
+
+    // We can even iterate through our row results and log to our console.
     data.rows.forEach(restaurant => {
       const { id, name, category, neighborhood } = restaurant
       console.log(`${id}. ${name} serves the best ${category} in ${neighborhood}.`)
     })
-  } catch (error) {
-    console.log('Error: ', error);
+  }
+
+  // Catch any errors that may occur from the query
+  catch (error) {
+    console.log('---- fetchRestaurants error ----\n', error);
+  }
+
+  // Regardless of success/failure, close the connection (see documentation)
+  finally {
+    client
+      .end()
+      .then(() => console.log('\n---- Disconnected ----\n'))
+      .catch(err => console.error('---- Error during disconnection ----\n', err.stack))
   }
 };
 
-fetchRestaurants();
+fetchRestaurants()
